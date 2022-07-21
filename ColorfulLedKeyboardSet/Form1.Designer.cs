704c94c362be87809c27723735300c7568dcbdac
moshuiD <68362682+moshuiD@users.noreply.github.com>
2022-07-21 15:53:32 +0800

    Add files via upload

---
+++ ColorfulLedKeyboardSet/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ColorfulLedKeyboardSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.speedBar = new System.Windows.Forms.TrackBar();
+            this.CustomRGB_B = new System.Windows.Forms.Button();
+            this.GetSource_L = new System.Windows.Forms.LinkLabel();
+            this.information_B = new System.Windows.Forms.Button();
+            this.ColorTestLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RGB循环";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // speedBar
+            // 
+            this.speedBar.LargeChange = 7;
+            this.speedBar.Location = new System.Drawing.Point(198, 5);
+            this.speedBar.Maximum = 7;
+            this.speedBar.Minimum = 1;
+            this.speedBar.Name = "speedBar";
+            this.speedBar.Size = new System.Drawing.Size(113, 56);
+            this.speedBar.TabIndex = 1;
+            this.speedBar.Value = 1;
+            // 
+            // CustomRGB_B
+            // 
+            this.CustomRGB_B.Location = new System.Drawing.Point(12, 67);
+            this.CustomRGB_B.Name = "CustomRGB_B";
+            this.CustomRGB_B.Size = new System.Drawing.Size(97, 34);
+            this.CustomRGB_B.TabIndex = 2;
+            this.CustomRGB_B.Text = "自定义RGB";
+            this.CustomRGB_B.UseVisualStyleBackColor = true;
+            this.CustomRGB_B.Click += new System.EventHandler(this.CustomRGB_B_Click);
+            // 
+            // GetSource_L
+            // 
+            this.GetSource_L.AutoSize = true;
+            this.GetSource_L.Location = new System.Drawing.Point(317, 77);
+            this.GetSource_L.Name = "GetSource_L";
+            this.GetSource_L.Size = new System.Drawing.Size(97, 15);
+            this.GetSource_L.TabIndex = 3;
+            this.GetSource_L.TabStop = true;
+            this.GetSource_L.Text = "获取程序源码";
+            this.GetSource_L.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GetSource_L_LinkClicked);
+            // 
+            // information_B
+            // 
+            this.information_B.Location = new System.Drawing.Point(198, 68);
+            this.information_B.Name = "information_B";
+            this.information_B.Size = new System.Drawing.Size(97, 34);
+            this.information_B.TabIndex = 4;
+            this.information_B.Text = "关于程序";
+            this.information_B.UseVisualStyleBackColor = true;
+            this.information_B.Click += new System.EventHandler(this.information_B_Click);
+            // 
+            // ColorTestLabel
+            // 
+            this.ColorTestLabel.AutoSize = true;
+            this.ColorTestLabel.Location = new System.Drawing.Point(317, 22);
+            this.ColorTestLabel.Name = "ColorTestLabel";
+            this.ColorTestLabel.Size = new System.Drawing.Size(97, 15);
+            this.ColorTestLabel.TabIndex = 5;
+            this.ColorTestLabel.Text = "您的键盘颜色";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "停止循环";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 114);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ColorTestLabel);
+            this.Controls.Add(this.information_B);
+            this.Controls.Add(this.GetSource_L);
+            this.Controls.Add(this.CustomRGB_B);
+            this.Controls.Add(this.speedBar);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "欢迎使用-作者墨水";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TrackBar speedBar;
+        private System.Windows.Forms.Button CustomRGB_B;
+        private System.Windows.Forms.LinkLabel GetSource_L;
+        private System.Windows.Forms.Button information_B;
+        private System.Windows.Forms.Label ColorTestLabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ColorfulLedKeyboardSet/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorfulLedKeyboardSet
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("InsydeDCHU.dll")]
+        public static extern int SetDCHU_Data(int command, byte[] buffer, int length);
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        public void SetColor(int KbPart, Color _color)
+        {
+            int num = 0;
+            switch (KbPart)
+            {
+                case 1:
+                    num = 240;
+                    break;
+                case 2:
+                    num = 241;
+                    break;
+                case 3:
+                    num = 242;
+                    break;
+                case 7:
+                    num = 246;
+                    break;
+                case 8:
+                    num = 243;
+                    break;
+            }
+            uint num2;
+            num2 = (uint)((int)_color.B << 16 | (int)_color.R << 8 | (int)_color.G);
+            if (_color.R == 0 && _color.G == 255 && _color.B == 127)
+            {
+                num2 = (uint)(4587520 | (int)_color.R << 8 | (int)_color.G);
+            }
+
+
+            byte[] bytes = BitConverter.GetBytes((long)((long)num << 24) + (long)((ulong)num2));
+            SetDCHU_Data(103, bytes, 4);
+            ColorTestLabel.ForeColor = _color;
+        }
+        int r = 255;
+        int g = 0;
+        int b = 0;
+        Thread LoopThread;
+        private void RGBLoop()
+        {
+            while (true)
+            {
+                if (!(g == 255 && r == 0 && b == 255))
+                {
+                    for (; g < 255; g += speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    g = g > 255 ? g = 255 : g;
+                    for (; r > 0; r -= speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    r = r < 0 ? r = 0 : r;
+                    for (; b < 255; b += speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    b = b > 255 ? b = 255 : b;
+                }
+                else
+                {
+                    for (; g > 0; g -= speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    g = g < 0 ? g = 0 : g;
+                    for (; r < 255; r += speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    r = r > 255 ? r = 255 : r;
+                    for (; b > 0; b -= speedBar.Value)
+                    {
+
+                        SetColor(1, Color.FromArgb(r, g, b));
+                        SetColor(2, Color.FromArgb(r, g, b));
+                        SetColor(3, Color.FromArgb(r, g, b));
+                    }
+                    b = b < 0 ? b = 0 : b;
+                }
+            }
+        }
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoopThread = new Thread(new ThreadStart(() => this.RGBLoop()));
+            LoopThread.IsBackground = true;
+            LoopThread.Start();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(Application.StartupPath + "\\InsydeDCHU.dll"))
+            {
+                MessageBox.Show("发生错误:InsydeDCHU.dll缺失\r\n，请检查程序运行文件夹下是否有InsydeDCHU.dll", "发生错误");
+                Environment.Exit(0);
+            }
+            MessageBox.Show("此程序为墨水制作\r\n利用逆向手段获取API编写而成\r\n有任何硬件问题开发者不承担任何责任！", "免责声明");
+            button2.Enabled = false;
+        }
+
+        private void CustomRGB_B_Click(object sender, EventArgs e)
+        {
+            if(LoopThread!=null)
+            LoopThread.Abort();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                SetColor(1, colorDialog1.Color);
+                SetColor(2, colorDialog1.Color);
+                SetColor(3, colorDialog1.Color);
+            }
+        }
+
+        private void information_B_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("你好，我是墨水\r\n这个程序是写给七彩虹笔记本（有可能神舟也可以）用户的\r\n" +
+                 "由于我发现这款电脑虽能设置键盘灯光\r\n但无法RGB循环以及自定义RGB\r\n故诞生了此程序\r\n" +
+                 "使用循环RGB模式可以拖动滑动条调整循环速度\r\n更多信息前往此项目Github仓库查看!\r\n" +
+                 "是否自动打开此程序Github仓库？", "一些信息", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Process.Start("https://github.com/moshuiD/Colorful-Keyborad-Led-Color-Setting");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoopThread.Abort();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void GetSource_L_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/moshuiD/Colorful-Keyborad-Led-Color-Setting");
+        }
+    }
+}
